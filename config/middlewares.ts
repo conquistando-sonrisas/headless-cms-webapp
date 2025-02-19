498a549a6a3233ad861fcfd88a2af6f189fe9d3e
Luis Cruz <luisccc99@gmail.com>
2025-02-18 22:39:08 -0600

    Agregar plugin para subir archivos a Amazon S3

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,30 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", "'https:'"],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'static-content-lccc.s3.us-east-1.amazonaws.com'
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'static-content-lccc.s3.us-east-1.amazonaws.com'
+          ],
+          upgradeInsecureRequests: null
+        }
+      }
+    }
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
