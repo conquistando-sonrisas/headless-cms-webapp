132069c4a16d78b24070154c50d1e9188ca01be6
Luis Cruz <luisccc99@gmail.com>
2025-06-04 21:10:30 -0600

    Utilizar local upload para guardar imagenes de manera local y no en s3

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,35 +1,19 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", "'https:'"],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'static-content-lccc.s3.us-east-1.amazonaws.com'
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'static-content-lccc.s3.us-east-1.amazonaws.com'
-          ],
-          upgradeInsecureRequests: null
-        }
-      }
-    }
-  },
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: "strapi::body",
+    config: {
+      formidable: {
+        maxFileSize: 20 * 1024 * 1024
+      },
+    },
+  },
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,23 +1,14 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: 'aws-s3',
+      sizeLimit: 20 * 1024 * 1024, // 256mb in bytes
+      provider: 'local',
       providerOptions: {
-        s3Options: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
-          params: {
-            ACL: env('AWS_ACL', 'public-read'),
-            signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-            Bucket: env('AWS_BUCKET')
-          },
-          credentials: {
-            accessKeyId: env('AWS_ACCESS_KEY_ID'),
-            secretAccessKey: env('AWS_ACCESS_SECRET')
-         }
-        }
-      }
+        sizeLimit: 20_000_000,
+        localServer: {
+          maxage: 300_000
+        },
+      },
     }
   }
 });
