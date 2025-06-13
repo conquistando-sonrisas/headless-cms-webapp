cd2deef53c31a2fde821fc252245f82e014c295f
Luis Cruz <luisccc99@gmail.com>
2025-06-12 18:40:01 -0600

    Eliminar el fetching de la cuenta que creo una novedad y actualizar env de ejemplo, eliminar variables de AWS

--- src/api/novedad/routes/novedad.ts
+++ src/api/novedad/routes/novedad.ts
@@ -4,13 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::novedad.novedad', {
-  config: {
-    find: {
-      middlewares: ['api::novedad.populate-authors']
-    },
-    findOne: {
-      middlewares: ['api::novedad.populate-authors']
-    }
-  }
-});
+export default factories.createCoreRouter('api::novedad.novedad');
